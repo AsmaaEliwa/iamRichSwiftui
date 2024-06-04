c493e924b13913f9359c8672b46b14492e5fcdb1
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2024-06-04 11:57:18 -0400

    starting app

---
+++ iamRichSwiftui/iamRichSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  iamRichSwiftuiApp.swift
+//  iamRichSwiftui
+//
+//  Created by asmaa gamal  on 04/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct iamRichSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
